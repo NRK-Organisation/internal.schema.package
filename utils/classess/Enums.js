fffa17959b6fda6aabcfb80ca6dc6caf458e45fc
Naman Khater <naman22khater@gmail.com>
2024-05-31 02:33:07 +0530

    utils updated

---
+++ utils/classess/Enums.js
@@ -0,0 +1,6 @@
+class Enums {
+    static ENTERPRISE_STATUS_KEY = ['Live', 'Demo', 'Inactive', 'ReadyForLive', 'ReadyForDemo', 'ReadyForInactive', 'ReadyForDelete', 'Deleted'];
+    static ENTERPRISE_STATUS = ['Live', 'Demo', 'Inactive', 'Ready For Live', 'Ready For Demo', 'Ready For Inactive', 'Ready For Delete', 'Deleted'];
+};
+
+module.exports = Enums;
