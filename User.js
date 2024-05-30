0d3d748a8a796768bef5a5274e448ebc8229ca36
Naman Khater <naman22khater@gmail.com>
2024-05-31 03:28:35 +0530

    updating user schema

--- User.js
+++ User.js
@@ -32,6 +32,14 @@ const UserSchema = new Schema({
             type: String,
             required: true
         }
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
     }
 }, {
     timestamps: true
